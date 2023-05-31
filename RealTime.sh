ae84b25871a5359cb50a50b610906d4d53a28257
Skitin <valentin.roch@epfl.ch>
2023-05-31 23:41:03 +0200

    Dirty update has been cleaned

--- RealTime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#conda activate dlav
-
-cd RealTime
-python validate_evl.py --ckpt_path '../ckpt/hemlets_h36m_lastest.pth' --dataset_path '../data/S11/S_11_C_4_1.h5' --visualize 1
--- inference.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#conda activate dlav
-cd DLAV
-python inference.py --ckpt_path '../ckpt/hemlets_h36m_lastest.pth' --dataset_path '../data/S11/S_11_C_4_1.h5' --visualize 1
